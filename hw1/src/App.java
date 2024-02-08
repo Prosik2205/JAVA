09a2bd19ee81fe4c898d1b95cd690c451bf3d630
Prosik2205 <116355710+Prosik2205@users.noreply.github.com>
2024-02-08 13:19:33 +0200

    123

--- hw1/src/App.java
+++ hw1/src/App.java
@@ -1,5 +1,61 @@
+
+
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    
+    
+    public static class first
+    {
+        int f = 5;
+        void print()
+        {
+            System.out.print(f);
+        }
+    }
+
+    public static class second {
+
+        int s = 5;
+        
+        void print()
+        {
+            System.out.print(s);
+        }
+        
+    }
+
+
+    
+    
+    public static void main(String[] args) throws Exception
+    {
+        first F;
+        second S;
+
+        
+
+     
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
