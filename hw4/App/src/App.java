ba5ff6ed553f4e18fb497398dc90c5e3764a5fed
Prosik2205 <116355710+Prosik2205@users.noreply.github.com>
2024-04-14 19:47:16 +0300

    done

--- hw4/App/src/App.java
+++ hw4/App/src/App.java
@@ -1,51 +1,47 @@
 public class App {
-    
-    public class CocktailSort {
-        void cocktailSort(int a[]) {
-            boolean swapped = true;
-            int start = 0;
-            int end = a.length;
-    
-            while (swapped == true) {
-                swapped = false;
-    
-                for (int i = start; i < end - 1; ++i) {
-                    if (a[i] > a[i + 1]) {
-                        int temp = a[i];
-                        a[i] = a[i + 1];
-                        a[i + 1] = temp;
-                        swapped = true;
-                    }
+    void cocktailSort(int a[]) {
+        boolean swapped = true;
+        int start = 0;
+        int end = a.length;
+
+        while (swapped == true) {
+            swapped = false;
+
+            for (int i = start; i < end - 1; ++i) {
+                if (a[i] > a[i + 1]) {
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    swapped = true;
                 }
-    
-                if (swapped == false)
-                    break;
-    
-                swapped = false;
-    
-                end = end - 1;
-    
-                for (int i = end - 1; i >= start; i--) {
-                    if (a[i] > a[i + 1]) {
-                        int temp = a[i];
-                        a[i] = a[i + 1];
-                        a[i + 1] = temp;
-                        swapped = true;
-                    }
-                }
-    
-                start = start + 1;
             }
+
+            if (swapped == false)
+                break;
+
+            swapped = false;
+
+            end = end - 1;
+
+            for (int i = end - 1; i >= start; i--) {
+                if (a[i] > a[i + 1]) {
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            start = start + 1;
         }
-
-
+    }
     public static void main(String[] args) throws Exception {
-        CocktailSort cs = new CocktailSort();
+        App cs = new App();
         int a[] = {5, 1, 4, 2, 8, 0, 2};
         cs.cocktailSort(a);
         System.out.println("Sorted array:");
         for (int i = 0; i < a.length; i++)
             System.out.print(a[i] + " ");
     }
-}
-}
+    }
+
